6866b44a60dca8e4fabee837901c8b8329f74f3e
gregoryAlvim <gregori.jagua@gmail.com>
2020-08-25 18:00:10 -0300

    Fundamentos Js

--- EXERCICIOS-JS/fundamentos/variaveisEConstantes.js
+++ EXERCICIOS-JS/fundamentos/variaveisEConstantes.js
@@ -1,3 +1,7 @@
 var a = 3
-let b = 4 //Criar o abito de usar Let, porque é mais moderno
-const c = 5 //Usar quando não for algo variável mais algo que sempre sera constante, como um valor único
+
+//Criar o abito de usar Let, porque é mais moderno
+let b = 4
+
+//Usar quando não for algo variável mais algo que sempre sera constante, como um valor único
+const c = 5

da9682caef858e3cc42dde2b938065f680d0a3f3
gregoryAlvim <gregori.jagua@gmail.com>
2020-09-16 16:44:52 -0300

    Delete

--- EXERCICIOS-JS/fundamentos/comentario.js
+++
@@ -1,8 +0,0 @@
-//Comentário de uma linha
-console.log('linha 1')
-
-/*
-    Comentário de
-    múltiplas linhas    
-*/
-console.log('linha 2')
--- EXERCICIOS-JS/fundamentos/usandoLetEmLoop1.js
+++
@@ -1,5 +0,0 @@
-for (let i = 0; i < 10; i++) {
-    console.log(i)
-}
-
-//console.log('i =', i) Gera um erro, porque a variável let se encontra só dentro do esboço do for
